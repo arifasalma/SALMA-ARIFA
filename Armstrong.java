283071f555e1c05140738e35f0bae94d4e336597
arifasalma <47441485+arifasalma@users.noreply.github.com>
2021-06-12 22:49:57 -0800

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+package hit1.java;
+
+public class Armstrong {
+	public static void main (String[] args) {
+		int number=371,originalnumber,remainder,result=0;
+		originalnumber=number;
+		while(originalnumber!=0)
+		{
+			remainder=originalnumber%10;
+			result +=Math.pow(remainder, 3);
+			originalnumber/=10;
+			}
+		if(result==number)
+			System.out.println(number+ " is an Armstrong number");
+		else
+			System.out.println(number+ " is not an Armstrong number.");
+		
+	}
+
+}
---
+++ SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+package hit1.java;
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int first=20, second=220;
+		System.out.println("--Before swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+		int temporary=first;
+		first=second;
+		second=temporary;
+		System.out.println("--After swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+		
+	}
+
+}
